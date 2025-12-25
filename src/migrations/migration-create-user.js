dd2dc0d1affc5269298f968657628827e3cdcdb3
DMNhut <ct7sxhctqk@privaterelay.appleid.com>
2025-12-25 18:01:20 +0700

    6pm 25/12/2025

--- src/migrations/migration-create-user.js
+++ src/migrations/migration-create-user.js
@@ -2,11 +2,24 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('Users', {
+    //   id: DataTypes.STRING,
+    // email: DataTypes.STRING,
+    // firstName: DataTypes.STRING,
+    // lastName: DataTypes.STRING,
+    // address: DataTypes.STRING,
+    // gender: DataTypes.BOOLEAN,
+    // roleid: DataTypes.STRING
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
       },
       firstName: {
         type: Sequelize.STRING
@@ -14,8 +27,17 @@ module.exports = {
       lastName: {
         type: Sequelize.STRING
       },
-      email: {
+      address: {
         type: Sequelize.STRING
+      },
+      gender: {
+          type: Sequelize.BOOLEAN
+      },
+      typeRole: {
+          type: Sequelize.STRING
+      },
+      keyRole: {
+          type: Sequelize.STRING
       },
       createdAt: {
         allowNull: false,
